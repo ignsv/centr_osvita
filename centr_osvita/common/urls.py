4cba637863deafd37eb0ae9db5ffb167b88dbd8d
Alex Ihnatenko <krylov1295@gmail.com>
2020-05-27 01:37:56 +0300

    Insert frontend structure into the project

--- centr_osvita/common/urls.py
+++ centr_osvita/common/urls.py
@@ -2,10 +2,15 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
-
-from .views import HealthCheckView
-
+from . import views
 
 urlpatterns = [
-    url(r'^checker/$', HealthCheckView.as_view()),
+    url(r'^$', views.HomePageView.as_view(), name='home'),
+    url(r'^teachers/$', views.OurTeachersView.as_view(), name='teachers'),
+    url(r'^statistics/$', views.StatisticsView.as_view(), name='statistics'),
+    url(r'^nice-moment/$', views.NiceMomentView.as_view(), name='nice_moment'),
+    url(r'^alisa/$', views.CenterAlisaView.as_view(), name='center_alisa'),
+    url(r'^zno-advices/$', views.ZNOAdvicesView.as_view(), name='zno_advices'),
+    url(r'^contacts/$', views.ContactsView.as_view(), name='contacts'),
+    url(r'^checker/$', views.HealthCheckView.as_view()),
 ]
