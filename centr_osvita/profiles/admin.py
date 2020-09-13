bcae72ac0867c3842acb1ab30e2a32bc93eb86e3
Oleksandr Ihnatenko <ihnatenko.oleksandr@steelkiwi.com>
2020-09-13 22:52:14 +0300

    Add user report functionality

--- centr_osvita/profiles/admin.py
+++ centr_osvita/profiles/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from centr_osvita.profiles.models import Profile, UserReport
 
-from centr_osvita.profiles.models import Profile
+
+@admin.register(UserReport)
+class UserReportAdmin(admin.ModelAdmin):
+    list_display = ('created',)
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'user')
+    list_display = ('full_name', 'user', 'created', 'institution_type', 'institution_name', 'grade')
+    actions = ['form_report']
+
+    def form_report(self, request, queryset):
+        report = UserReport.objects.create()
+        report.profiles = queryset
+        report.save()
+    form_report.short_description = "Form report"
